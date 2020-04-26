b4d03d9b35348d1e790b41cb1e3a1f7e04b2eddf
arata-hidano <aratahidano@gmail.com>
2020-04-26 17:26:00 +0700

    SImplified

--- Cambodia_model/codes/0_master_Cam_preliminary.r
+++ Cambodia_model/codes/0_master_Cam_preliminary.r
@@ -1,54 +1,35 @@
-# Specify if country as one pop model shuld run
+# Choose models to run
+# Running Cambodia as one population? Choose 1 if yes otherwise 0
 one_country_model = 0
-fig3 = 1
-same_beta = 0
-calculate_R = 1
-# load relevant the data files
-source('codes/1_loadData.r')
-source('codes/1_loadOpenProb.r')
-
-
-# source the age-structured SEIcIscR model functions 
-source('codes/function_modelSEIcIscRBCZ.r')
-source('codes/function_modelSEIcIscRBCZ_simultaneous.r')
-
-
-# simulate N oubtreaks
-source('codes/2_simOutbreak_ncov_SEIcIscRBCZ.r')
-source('codes/2_simOutbreak_ncov_SEIcIscRBCZ_simultaneous.r')
-source('codes/2_parallel_simOutbreak_ncov_SEIcIscRBCZ.r')
-
-rm(list=ls())
-# Fig 3 same_beta = 1
-one_country_model = 0
-fig3 = 1
-same_beta = 1
-calculate_R = 1
-# load relevant the data files
-source('codes/1_loadData.r')
-source('codes/1_loadOpenProb.r')
-
-
-# source the age-structured SEIcIscR model functions 
-source('codes/function_modelSEIcIscRBCZ.r')
-
-
-# simulate N oubtreaks
-source('codes/2_parallel_simOutbreak_ncov_SEIcIscRBCZ.r')
-# save figures
-source('check_codes/paper_fig3_5.r')
-rm(list=ls())
-# National trigger model
-# same_beta = 0
-one_country_model = 0
+# Producing figure 3? Choose 1 if yes otherwise 0
 fig3 = 0
+# Use same beta (calculated based on Phnom Penh contact) for all provinces? Choose 1 if yes otherwise 0
 same_beta = 0
+# Do you need to calculate R0? Choose 1 if yes otherwise 0
 calculate_R = 0
+
+# load relevant the data files
 source('codes/1_loadData.r')
 source('codes/1_loadOpenProb.r')
 
 
-# source the age-structured SEIcIscR model functions 
+# source the age-structured SEIcIscR model functions. Choose one from below.
+# If running each province separately without implementing National-trigger model, choose this
+source('codes/function_modelSEIcIscRBCZ.r')
+# If running all provinces simultaneously with/without implementing National-trigger model, choose this
 source('codes/function_modelSEIcIscRBCZ_simultaneous.r')
-# simulate N oubtreaks
+
+
+# simulate N oubtreaks - choose one from below
+#source('codes/2_simOutbreak_ncov_SEIcIscRBCZ.r') # obsolete: not updating anymore
+
+# If running each province separately without implementing National-trigger model, choose this
+source('codes/2_parallel_simOutbreak_ncov_SEIcIscRBCZ.r')
+
+# If running all provinces simultaneously with/without implementing National-trigger model, choose this
 source('codes/2_simOutbreak_ncov_SEIcIscRBCZ_simultaneous.r')
+
+
+
+
+
